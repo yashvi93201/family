cc9ea3322ab6b0cde8678c305a0496755c7bc598
yashvi93201 <73698192+yashvi93201@users.noreply.github.com>
2020-11-11 20:04:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var familyarry=["https://tse3.mm.bing.net/th?id=OIP.AlTlmKGOra4GFz_697wx1wHaS1&pid=Api&P=0&w=300&h=300","https://webstockreview.net/images/father-clipart-10.jpg",
+ "https://tse3.explicit.bing.net/th?id=OIP.ScaGOXitPka4DFmxgzwyiwHaF_&pid=Api&P=0&w=198&h=161",
+"https://tse2.mm.bing.net/th?id=OIP.PWRgzJsFQ4xpMl1KkBh4VQHaGl&pid=Api&P=0&w=178&h=159"];
+var namesarry=["Divya varshney","Navdeep Varshney","Pranaya Varshney","Yashvi Varshney"];
+var i=0;
+function a(){
+document.getElementById("album").src=familyarry[i];
+document.getElementById("name").innerHTML=namesarry[i];
+i++;
+}
